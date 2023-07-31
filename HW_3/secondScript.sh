0860b518dad27377f711a54b0fb24234b02d84d2
Kirill Mokeichev <kirillmok78@gmail.com>
2023-07-31 22:40:16 +0300

    One more change in script for Validat-Shell

--- HW_3/secondScript.sh
+++ HW_3/secondScript.sh
@@ -5,12 +5,12 @@ sudo useradd -m $1
 touch /home/$1/systemInformation
 
 uname -r >> /home/$1/systemInformation
-echo '##############################' >>/home/$1/systemInformation
+echo '##############################' >> /home/$1/systemInformation
 
 lscpu >> /home/$1/systemInformation
-echo '##############################' >>/home/$1/systemInformation
+echo '##############################' >> /home/$1/systemInformation
 
 free -h >> /home/$1/systemInformation
-echo '##############################' >>/home/$1/systemInformation
+echo '##############################' >> /home/$1/systemInformation
 
-df >>/home/$1/systemInformation
+df >> /home/$1/systemInformation
