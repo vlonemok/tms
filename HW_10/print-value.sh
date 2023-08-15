782e5b0ce9e5ba2e5251a31477bc431002c88778
Kirill Mokeichev <kirillmok78@gmail.com>
2023-08-15 14:48:05 +0300

    Finished print-value.sh

--- HW_10/print-value.sh
+++ HW_10/print-value.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo $1
+if [[ $1 ]]; then
+    VALUE=$1
+    echo "$VALUE"
+
+elif [[ $VALUE ]]; then
+    echo "$VALUE"
+
+elif [[ $1 -eq "" ]] || [[ $VALUE -eq "" ]]; then
+    echo "Вы ничего не ввели"
+
+else
+    echo "Вы ввели неверные данные"
+    exit 1
+fi
