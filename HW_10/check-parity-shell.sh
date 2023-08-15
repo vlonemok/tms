cc7665e61e2954ce3099a5945aa01e4f7423fde2
Kirill Mokeichev <kirillmok78@gmail.com>
2023-08-15 15:31:35 +0300

    Finished check-parity-shell.sh script

--- HW_10/check-parity-shell.sh
+++ HW_10/check-parity-shell.sh
@@ -1,1 +1,14 @@
-#!/bin/bash
+#!/usr/bin/sh
+
+numToCheck=$1
+
+if [ $((numToCheck % 2)) -eq 0 ]; then
+    echo "Число является четным"
+
+elif [ $((numToCheck % 2)) -ne 0 ]; then
+    echo "Число является нечетным"
+
+else
+    echo "Вы ввели неверное число"
+    exit 1
+fi
