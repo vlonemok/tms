c7527a3a99e5a36087c4c5af7bcb2993bf8fcdd0
Kirill Mokeichev <kirillmok78@gmail.com>
2023-08-15 21:22:34 +0300

    Changes in scripts due to ShellCheck

--- HW_10/generate-testing-matrix.sh
+++ HW_10/generate-testing-matrix.sh
@@ -4,7 +4,7 @@ os=("Windows" "Linux" "MacOS")
 arch=("x86" "x64" "arm" "x86_64")
 
 for i in "${os[@]}"; do
-    for j in "${arch[@]}"; do
-        echo "$i" - "$j"
-    done
+        for j in "${arch[@]}"; do
+            echo "$i" - "$j"
+        done
 done
