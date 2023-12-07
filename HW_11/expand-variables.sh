d483fd9e859969c7d2f635dfecdbeee9363096eb
Kirill Mokeichev <kirillmok78@gmail.com>
2023-12-07 16:34:38 +0300

    Finished first and second task

--- HW_11/expand-variables.sh
+++ HW_11/expand-variables.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+while getopts ":f:" opt; do
+  case $opt in
+    f)
+      file="$OPTARG"
+      ;;
+    \?)
+      echo "Неверный аргумент: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+key_value_pair="$*"
+
+if [ -n "$file" ]; then
+  template=$(cat "$file")
+else
+  template=$(cat)
+fi
+
+IFS=' ' read -ra vars <<< "$key_value_pair"
+for var in "${vars[@]}"; do
+  export "$var"
+done
+
+result=$(echo "$template" | envsubst)
+
+echo "$result"
--- HW_11/filter.sh
+++ HW_11/filter.sh
@@ -1,1 +1,49 @@
 #!/bin/bash
+
+while getopts ":f:" opt; do
+  case $opt in
+    f)
+      file="$OPTARG"
+      ;;
+    \?)
+      echo "Неверный аргумент: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+rule="$1"
+shift
+
+if [ -n "$file" ]; then
+  lines=$(cat "$file")
+else
+  lines=$(cat)
+fi
+
+filter_lines() {
+  local rule="$1"
+  local token="$2"
+  local lines="$3"
+
+  case "$rule" in
+    contains)
+      echo "$lines" | grep "$token"
+      ;;
+    startswith)
+      echo "$lines" | grep "^$token"
+      ;;
+    endswith)
+      echo "$lines" | grep "$token$"
+      ;;
+    *)
+      echo "Неверное правило: $rule. Поддерживаемые правила: contains, startswith, endswith." >&2
+      exit 1
+      ;;
+  esac
+}
+
+filtered_lines=$(filter_lines "$rule" "$1" "$lines")
+
+echo "$filtered_lines"
